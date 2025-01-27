cd407ade3880904f0dc069972d0df8eb68d889f4
Malleshwari4 <pallikondamalleshwari@gmail.com>
2025-01-27 15:46:55 +1300

    first commit

---
+++ src/test/java/com/maven/ManualTesting.java
@@ -0,0 +1,11 @@
+package com.maven;
+
+public class ManualTesting {
+
+	public static void main(String[] args) {
+		System.out.println("Define Manual Testing?");
+		System.out.println("Manual testing is a software testing method where a human tester interacts with a software application to find bugs and other issues");
+
+	}
+
+}
